2b7643816b7055f6e8b1174ff5ea0cbb830392bb
DilshodIskandarov456 <76392416+DilshodIskandarov456@users.noreply.github.com>
2022-03-16 16:30:19 +0500

    Initial Commit

---
+++ weather_app_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather_app_test
+//
+//  Created by Dilshod Iskandarov on 3/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
